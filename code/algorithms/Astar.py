ff04dc0c69edf048810ebc974a31231605ba88da
TomKamstra <54835029+TomKamstra@users.noreply.github.com>
2020-01-24 16:44:06 +0100

    Update Astar.py

--- code/algorithms/Astar.py
+++ code/algorithms/Astar.py
@@ -5,18 +5,14 @@
 # 
 # Pseudocode source: http://mat.uab.cat/~alseda/MasterOpt/AStar-Algorithm.pdf
 ##################################################
-import time
-from operator import attrgetter
-
 class Grid():
     def __init__(self):
         self.grid = []
         self.g = 0
 
     def make_grid(self, blocked, start, goal, net):
-        # start_time_1 = time.time()
-        for x in range(-1, 5):
-            for y in range(-1, 5):
+        for x in range(-1, 17):
+            for y in range(-1, 12):
                 for z in range(8):
                     node = Node(x, y, z, net)
                     # if str(node) not in blocked:
@@ -26,9 +22,8 @@ class Grid():
                     self.grid.append(node)
                     if str(node) in blocked and str(node) != str(start) and str(node) != str(goal):
                         node.set_blocked()
-        
-        # end_time_1 = time.time()
-        # print("TIME 1: ", end_time_1 - start_time_1)
+    
+    
 
     def get_grid(self):
         return self.grid
@@ -50,11 +45,8 @@ class Node():
     def set_blocked(self):
         self.blocked = True
         
-    def set_f(self):
-        self.f = self.g + self.h
-
-    def get_f(self):
-        return self.f
+    def f_score(self):
+        return self.g + self.h
 
     def get_coordinate(self):
         return str([self.x, self.y, self.z])
@@ -67,7 +59,10 @@ class Node():
         # self.g = value + 8 - self.z
         self.g = value
     
-    def set_h(self, current, goal):
+    def g_score(self, value):
+        self.g += value
+    
+    def h_score(self, current, goal):
         self.h = abs(current.x - goal.x) + abs(current.y - goal.y) + abs(current.z - goal.z)
         
         return self.h
@@ -98,7 +93,28 @@ class Node():
                         if j not in closed_list and str(j) != str(start):
                             if not j.get_blocked():
                                 self.node_successors.add(j)
+       
+        # print("before delete: ", self.node_successors)
         
+        
+        # coordinates = []
+        # for coordinate in blocked: 
+        #     coordinates.append(coordinate.get_coordinate())
+
+        # for successor in self.node_successors:
+        #     if successor.get_coordinate() in coordinates:
+        #         if successor.get_coordinate() == [2, 1, 0]:
+        #             print('check', coordinates)
+        #             # input()
+        #         if str(successor) != str(start) and str(successor) != str(goal):
+        #             print(successor.get_coordinate())
+        #             self.node_successors.remove(successor)
+        # print("BEFORE: ", self.node_successors)
+
+        # self.node_successors = self.node_successors - set(blocked)
+        # print("AFTER: ", self.node_successors)
+        # print(coordinates)
+        # input('end')
         return self.node_successors
 
     def __repr__(self):
@@ -108,71 +124,37 @@ class Node():
 def search(open_list, closed_list, blocked, grid, start, goal):
     # print(open_list, closed_list, blocked, start, goal)
     # input()
-
     while open_list:
-        # start_time_3 = time.time()
-        # f_list = []
-        # for i in open_list:
-        #     f_list.append(i.f_score())
-        # minimum = min(f_list)
-        # for i in range(len(f_list)):
-        #     if f_list[i] == minimum:
-        #         q = open_list[i]
-
-        # Kan sneller
+        f_list = []
         for i in open_list:
-            i.set_f()
+            f_list.append(i.f_score())
+        minimum = min(f_list)
+        for i in range(len(f_list)):
+            if f_list[i] == minimum:
+                q = open_list[i]
 
-            # snellere code als start automatisch 1 omhoog gaat?
-            
-        q = min(open_list, key=attrgetter('f'))
         
-        # end_time_3 = time.time()
-        # print("TIME 3: ", end_time_3 - start_time_3)
-
         if str(q) == str(goal):
             print("finished")
             break
-        
-        # start_time_4 = time.time()
+
         successors = q.successors(grid, q, closed_list, blocked, start, goal)
-        # end_time_4 = time.time()
-        # print("TIME 4: ", end_time_4 - start_time_4)
         # print("Q", q, successors)
 
+        # print("successssss", q, successors)
         
-        # Om code sneller te maken: Forceer hem naar boven als z + 1 kan en x en y hetzelfde zijn als start
-        # if q.x == start.x and q.y == start.y and q.z != start.z:
-        #     pass
-        # for i in successors:
-        #     if i.x == start.x and i.y == start.y:
-        #         continue
-        #     else:
-        #         successors.remove(i)
-
-
-        # Dit maakt hem slomer
-        # if q.z < 6 and q.x == start.x and q.y == start.y:
-        #     successors = [item for item in successors if item.x == start.x and item.y == start.y]
-
-        # print("Q", q, successors)
+        # node_previous = q
+                    
+        # print("current: ", q)
         for i in successors:
-            # if i.x == start.x and i.y == start.y:
-            # start_time_5 = time.time()
-            
-            
 
             # Make it more expensive to traverse the x or y axis on lower layers
             weight = 1
-            if i.x != q.x or i.y != q.y:
-                weight += 8 - q.z
+            # if i.x != q.x or i.y != q.y:
+            #     weight += 8 - q.z
 
 
             successor_current_cost = q.get_g() + weight
-            # end_time_5 = time.time()
-            # print("TIME 5: ", end_time_5 - start_time_5)
-
-            # start_time_6 = time.time()
             if i in open_list:
                 if i.get_g() <= successor_current_cost:
                     i.set_g(successor_current_cost)
@@ -189,9 +171,7 @@ def search(open_list, closed_list, blocked, grid, start, goal):
             else:
                 # print("4")
                 open_list.append(i)
-                i.set_h(i, goal)
-            # end_time_6 = time.time()
-            # print("TIME 6: ", end_time_6 - start_time_6)
+                i.h_score(i, goal)
             # A star werkt niet als alles dezelfde kost heeft toch een stap terug moet extra kosten hebben doordat de huristiek daar groter wordt.
             # print("plus 1:::: ", successor_current_cost, i.h, i)
             
@@ -200,8 +180,6 @@ def search(open_list, closed_list, blocked, grid, start, goal):
          
         open_list.remove(q)
         closed_list.append(q)
-        
-        # input()
         
     return q
 
@@ -229,7 +207,7 @@ def a_star(start, goal, blocked, net):
     grid = initialize_grid(blocked, start, goal, net)
 
     # Calculate h for start node
-    start.set_h(start, goal)
+    start.h_score(start, goal)
 
     print(start.h)
 
@@ -237,10 +215,7 @@ def a_star(start, goal, blocked, net):
     open_list = [start]
 
     closed_list = []
-    # start_time_2 = time.time()
     q =  search(open_list, closed_list, blocked, grid, start, goal)
-    # end_time_2 = time.time()
-    # print("TIME 2: ", end_time_2 - start_time_2)
     
     return generate_path(q)
 
